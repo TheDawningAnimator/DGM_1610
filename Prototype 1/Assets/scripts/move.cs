785e7626a0a4395638fffe494909258da7bcf8e1
Dawn Smith <10943551@my.uvu.edu>
2021-01-27 16:38:46 -0700

    completed prototype 1

--- Prototype 1/Assets/scripts/move.cs
+++ Prototype 1/Assets/scripts/move.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class move : MonoBehaviour
 {
     // Access modifier, Data type, Name
-    public float speed = 5.0f;
+    public float speed = 30.0f;
+
+    private float turnspeed = 35.9f;
+
 
     public float hInput;
 
@@ -19,10 +22,12 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //gather inpt for controls
         hInput = Input.GetAxis("Horizontal");
         fInput = Input.GetAxis("Vertical");
-
+        //makes player go back and forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * fInput);
-        transform.Translate(Vector3.right * Time.deltaTime * speed * hInput);
+        //makes player go left and right
+        transform.Rotate(Vector3.up * turnspeed * hInput * Time.deltaTime);
     }
 }
