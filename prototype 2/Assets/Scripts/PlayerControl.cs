37717c314276925d2ac16626134f4680f9c11b67
Dawn Smith <10943551@my.uvu.edu>
2021-02-01 17:28:20 -0700

    added prototype 2

---
+++ prototype 2/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float hInput;
+    public float speed = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        if (transform.position.x < -15)
+            transform.position = new Vector3(-15, transform.position.y, transform.position.z);
+        transform.Translate(Vector3.right * hInput * Time.deltaTime * speed);
+    }
+}
