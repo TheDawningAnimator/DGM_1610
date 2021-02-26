5e45bd0de76ca5dacc052293132cc1978af7df8a
Dawn Smith <10943551@my.uvu.edu>
2021-02-26 13:59:28 -0700

    added spawn manger

--- prototype 3/Assets/Scripts/SpawnManager.cs
+++ prototype 3/Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject obsticalPrefab;
+
+    public float startDelay = 0.2f;
+
+    public float repeatDelay = 
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating();
+        Instantiate(obsticalPrefab, spawnPos, obsticalPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    void spawnObstical();
 }
