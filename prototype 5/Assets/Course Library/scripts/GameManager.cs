12c00f2e0dd053af0da5230dfcfa331ea4c23897
Dawn Smith <10943551@my.uvu.edu>
2021-04-21 16:34:25 -0600

    add challenge 3 and prototype 5

--- prototype 5/Assets/Course Library/scripts/GameManager.cs
+++ prototype 5/Assets/Course Library/scripts/GameManager.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public List<GameObject> targets;
+
+    private float spawnRate = 1.0f;
+
+    private int score;
+
+    public TextMeshProUGUI scoreText;
+
+    public TextMeshProUGUI gameOverText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTarget());
     }
 
     // Update is called once per frame
-    void Update()
+    IEnumerable SpawnTarget()
     {
-        
+        while(isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+    public void UpdateScore(int scoreTotal)
+    {
+        score += scoreTotal;
+        scoreText.text = "score" + score;
+    }
+    public void GameOver()
+    {
+        gameOverText.gameobject.SetActive(true);
     }
 }
